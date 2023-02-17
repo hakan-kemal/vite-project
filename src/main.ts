1ccbf2e615ccdf6860ab54855798a520c2431d2f
Hakan Çuhadar <hakan@q42.nl>
2023-02-17 23:52:00 +0100

    Use vite-project id and fix incorrect index file

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './style.css';
 import App from './App.svelte';
 
 const app = new App({
-	target: document.getElementById('app'),
+	target: document.getElementById('vite-project'),
 })
 
 export default app
